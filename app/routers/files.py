1d2eddff16fd9feac6d427db445a3c16325eb864
toddn <tcnichol@illinois.edu>
2021-10-24 15:53:59 -0500

    can download files now

--- app/routers/files.py
+++ app/routers/files.py
@@ -5,6 +5,7 @@ import gridfs
 import motor
 from pymongo import MongoClient
 from gridfs import GridFS
+from starlette.responses import FileResponse
 from bson import ObjectId
 from fastapi import APIRouter, Request, HTTPException, Depends, File, UploadFile
 from app.models.datasets import Dataset
@@ -15,7 +16,9 @@ router = APIRouter()
 
 auth_handler = AuthHandler()
 
-
+client = MongoClient(os.environ["MONGODB_URL"])
+current_db = client['clowder']
+fs = GridFS(current_db)
 
 @router.post("/test/")
 async def test():
@@ -49,9 +52,6 @@ async def create_upload_file(request: Request, file: UploadFile = File(...)):
 @router.post("/uploadfileGridFs/")
 async def create_upload_file_gridfs(request: Request, file: UploadFile = File(...)):
     print('here')
-    client = MongoClient(os.environ["MONGODB_URL"])
-    current_db = client['clowder']
-    fs = GridFS(current_db)
     content = await file.read()
     filename = file.filename
     content_type = file.content_type
@@ -60,3 +60,28 @@ async def create_upload_file_gridfs(request: Request, file: UploadFile = File(..
     except Exception as e:
         print(e)
     return {"filename": file.filename}
+
+@router.get("/downloadAFile")
+async def download_test(request: Request):
+    most_recent_three = fs.find().sort("uploadDate", -1).limit(3)
+
+    for grid_out in most_recent_three:
+        filename = grid_out.filename
+        content_type = grid_out.content_type
+        data = grid_out.read()
+        with open(filename, 'wb') as f:
+            f.write(data)
+        file_path = os.path.join(os.getcwd(), filename)
+        if os.path.isfile(file_path):
+            # this serves the file
+            # return FileResponse(file_path)
+            # this downloads the file
+            return FileResponse(path=file_path, media_type='application/octet-stream', filename=filename)
+
+        print('here')
+        print('here')
+
+
+    # return FileResponse(file_location, media_type='application/octet-stream', filename=file_name)
+    return {"status":"ok"}
+
