187318691ade3d113f8948e0e2d9f5a884746217
toddn <tcnichol@illinois.edu>
2021-10-25 15:11:36 -0500

    removing unused methods

--- app/routers/files.py
+++ app/routers/files.py
@@ -21,30 +21,8 @@ client = MongoClient(os.environ["MONGODB_URL"])
 current_db = client['clowder']
 fs = GridFS(current_db)
 
-def cleanup(temp_file):
-    os.remove(temp_file)
-
 def remove_file(path: str) -> None:
     os.unlink(path)
-
-@router.post("/test/")
-async def test():
-    return {'ok':'test'}
-
-@router.get("/allfiles")
-async def get_all_files(request: Request):
-    files = []
-    skip = 0
-    limit = 300
-    for doc in await request.app.db["fileuploads"].find().skip(skip).limit(limit).to_list(length=limit):
-        files.append(doc)
-    return {'ok':'files'}
-
-@router.post("/files/")
-async def create_file(file: bytes = File(...)):
-    print('here')
-    return {"file_size": len(file)}
-
 
 @router.post("/upload/dataset/{dataset_id}")
 async def upload_file(dataset_id: str, request: Request, file: UploadFile = File(...)):
@@ -61,17 +39,6 @@ async def upload_file(dataset_id: str, request: Request, file: UploadFile = File
         print(e)
     return {"id": str(oid)}
 
-@router.post("/uploadfileGridFs/")
-async def create_upload_file_gridfs(request: Request, file: UploadFile = File(...)):
-    print('here')
-    content = await file.read()
-    filename = file.filename
-    content_type = file.content_type
-    try:
-        oid = fs.put(content, content_type=content_type, filename=filename)
-    except Exception as e:
-        print(e)
-    return {"filename": file.filename}
 
 @router.get("/downloadFile/{file_id}")
 async def download_test(file_id: str, request: Request, background_tasks: BackgroundTasks):
@@ -86,8 +53,5 @@ async def download_test(file_id: str, request: Request, background_tasks: Backgr
         if os.path.isfile(file_path):
             background_tasks.add_task(remove_file, file_path)
             return FileResponse(path=file_path, media_type='application/octet-stream', filename=filename)
-
-
-    # return FileResponse(file_location, media_type='application/octet-stream', filename=file_name)
-    return {"status":"ok"}
+    return {"status":"file not found"}
 
