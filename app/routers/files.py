a1fa62813c157a2bfd43be90150f4daed7663ceb
toddn <tcnichol@illinois.edu>
2021-10-29 13:30:17 -0500

    adding router for files

---
+++ app/routers/files.py
@@ -0,0 +1,54 @@
+import os
+from pymongo import MongoClient
+from gridfs import GridFS
+from starlette.responses import FileResponse
+from starlette.background import BackgroundTasks
+from bson import ObjectId
+from fastapi import APIRouter, Request, HTTPException, Depends, File, UploadFile
+from app.models.datasets import Dataset
+from ..auth import AuthHandler
+
+router = APIRouter()
+
+auth_handler = AuthHandler()
+
+client = MongoClient(os.environ["MONGODB_URL"])
+current_db = client['clowder']
+fs = GridFS(current_db)
+
+def remove_file(path: str) -> None:
+    os.unlink(path)
+
+@router.post("/upload/dataset/{dataset_id}")
+async def upload_file(dataset_id: str, request: Request, file: UploadFile = File(...)):
+    if (dataset := await request.app.db["datasets"].find_one({"_id": ObjectId(dataset_id)})) is not None:
+        current_dataset = Dataset.from_mongo(dataset)
+        content = await file.read()
+        filename = file.filename
+        content_type = file.content_type
+        try:
+            oid = fs.put(content, content_type=content_type, filename=filename)
+            updated_dataset = await request.app.db["datasets"].update_one({'_id': ObjectId(dataset_id)}, {
+                '$push': {'files': ObjectId(oid)}})
+        except Exception as e:
+            print(e)
+        return {"id": str(oid)}
+    else:
+        return {"status":"dataset not found"}
+
+
+@router.get("/downloadFile/{file_id}")
+async def download_test(file_id: str, request: Request, background_tasks: BackgroundTasks):
+    file = fs.find_one({"_id": ObjectId(file_id)})
+
+    if file is not None:
+        filename = file.filename
+        data = file.read()
+        with open(filename, 'wb') as f:
+            f.write(data)
+        file_path = os.path.join(os.getcwd(), filename)
+        if os.path.isfile(file_path):
+            background_tasks.add_task(remove_file, file_path)
+            return FileResponse(path=file_path, media_type='application/octet-stream', filename=filename)
+    return {"status": "file not found"}
+
